1a2ee87c6fcddd0a997b514831cd1d462fa0d2f9
idailylife <iorange@126.com>
2016-06-16 17:12:55 +0800

    sequenced task configuration

--- src/main/java/edu/inlab/service/assignment/SequenceMicroTaskAssigner.java
+++ src/main/java/edu/inlab/service/assignment/SequenceMicroTaskAssigner.java
@@ -1,8 +1,11 @@
 package edu.inlab.service.assignment;
 
 import edu.inlab.models.Microtask;
+import edu.inlab.models.Task;
 import edu.inlab.models.UserTask;
 import edu.inlab.service.MicroTaskService;
+import edu.inlab.service.TaskService;
+import org.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
@@ -18,13 +21,27 @@ public class SequenceMicroTaskAssigner implements MicroTaskAssigner {
     @Autowired
     MicroTaskService microTaskService;
 
+    @Autowired
+    TaskService taskService;
+
     @Override
     public Microtask assignNext(UserTask userTask) throws RuntimeException {
         Integer currMTaskId = userTask.getCurrUserMicrotaskId();
+        Task task = taskService.findById(userTask.getTaskId());
         if(currMTaskId != null){
             Microtask currMTask = microTaskService.getById(currMTaskId);
-            if(currMTask.getNextId() != null)
-                return microTaskService.getById(currMTask.getNextId());
+//            if(currMTask.getNextId() != null)
+//                return microTaskService.getById(currMTask.getNextId());
+            JSONArray mTaskArray = new JSONArray(task.getParams());
+            int i;
+            for(i=0; i<mTaskArray.length(); i++){
+                int mtId = mTaskArray.getInt(i);
+                if(currMTask.getId().equals(mtId))
+                    break;
+            }
+            if(i+1 < mTaskArray.length()){
+                return microTaskService.getById(mTaskArray.getInt(i+1));
+            }
         }
         return null;
     }
